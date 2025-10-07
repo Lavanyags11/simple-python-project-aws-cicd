4884cf909baca7d8c7315cb03a68df25764c39b8
Lavanyags11 <gslavanyagowda@gmail.com>
2025-10-07 23:49:12 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-# Get container IDs using port 5000
-containers=$(docker ps -q --filter "publish=5000")
-
-# Stop and remove if any found
-if [ -n "$containers" ]; then
-    docker rm -f $containers
-else
-    echo "No containers running on port 5000."
-fi
-
+# stop the container
+echo hi
 
